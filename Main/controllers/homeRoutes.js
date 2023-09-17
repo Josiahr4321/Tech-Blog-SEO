8c0217b57c5a9c0f0263ee0ee36812a520354b14
Joe Rivera <dvs_joe@hotmail.com>
2023-09-17 19:07:45 -0400

    got page to launch

--- Main/controllers/homeRoutes.js
+++ Main/controllers/homeRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
-const { Project, User } = require('../models');
+const { Post } = require('../models');
 const withAuth = require('../utils/auth');
+
 
 router.get('/', async (req, res) => {
   try {
--- Main/db/schema.sql
+++ Main/db/schema.sql
@@ -1,2 +1,5 @@
 DROP DATABASE IF EXISTS techblog_db;
 CREATE DATABASE techblog_db;
+
+GRANT ALL PRIVILEGES ON techblog_db* TO 'root'@'localhost';
+FLUSH PRIVILEGES;
